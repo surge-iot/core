ba8c6342f67b5d58c11df349dbbe402c097c7970
Shinjan Mitra <shinjanxp@gmail.com>
2020-05-15 11:57:22 +0530

    feat: Adds swagger module

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,14 @@ async function bootstrap() {
   }));
   app.setGlobalPrefix('api');
   app.enableCors();
+  const options = new DocumentBuilder()
+    .setTitle('Oneboard API')
+    .setDescription('The oneboard API description')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/specs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
